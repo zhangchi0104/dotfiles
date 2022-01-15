9dbf2500519c36e88e599da9756865331376a861
Alex Zhang <zhangchi0104@live.com>
2022-01-15 17:48:54 +0800

    fixed: hitokoto breaks when there is no cache

--- nvim-lua/hitokoto.lua
+++ nvim-lua/hitokoto.lua
@@ -13,37 +13,46 @@ local open = require'plenary.context_manager'.open
 
 local HITOKOTO_FILE = Path:new(vim.fn.stdpath('data')) / 'hitokoto.txt'
 local MAX_LINES = 10
+
 local function format_hitokoto(resp)
-    local hitokoto = resp.hitokoto
-    local from_who = ''
-    if type(resp.from_who) == 'string' then
-      from_who = ' ' .. resp.from_who
-    end
-  
-    local source = ' ' .. resp.from
-    return string.format('%s --%s《%s》', hitokoto, from_who, source)
+  local hitokoto = resp.hitokoto
+  local from_who = ''
+  if type(resp.from_who) == 'string' then
+    from_who = ' ' .. resp.from_who
+  end
+
+  local source = ' ' .. resp.from
+  return string.format('%s --%s《%s》', hitokoto, from_who, source)
+end
+
+local function fetch_hitokoto()
+  local resp = curl.get({
+    url = 'https://v1.hitokoto.cn/?c=a&encode=json',
+    accept = 'application/json'
+  })
+  local resp_json = vim.fn.json_decode(resp.body)
+  local hitokoto = format_hitokoto(resp_json)
+  return hitokoto
 end
 
 -- Selects a random quote from json
 function M.quote()
-  local lines = nil
-  with(open(HITOKOTO_FILE, 'r'), function(reader)
-    lines = vim.fn.split(reader:read('*all'), '\n')
-  end)
-  return lines[math.random(1, 10)]
+  if not HITOKOTO_FILE:exists() then
+    M.cache()
+    return fetch_hitokoto()
+  else
+    with(open(HITOKOTO_FILE, 'r'), function(reader)
+      lines = vim.fn.split(reader:read('*all'), '\n')
+    end)
+    return lines[math.random(1, 10)]
+  end
 end
-
 
 -- Caches 10 quotes to file
 function M.cache()
   local hito
   local wrapped =  a.wrap(function(l, cb)
-    local resp = curl.get({
-      url = 'https://v1.hitokoto.cn/?c=a&encode=json',
-      accept = 'application/json'
-    })
-    local resp_json = vim.fn.json_decode(resp.body)
-    local hitokoto = format_hitokoto(resp_json)
+    local hitokoto = fetch_hitokoto()
     table.insert(l, hitokoto)
     cb()
   end, 2)
@@ -56,8 +65,7 @@ function M.cache()
     end
     local s = table.concat(hitokotos, '\n')
     with(open(HITOKOTO_FILE, 'w'), function(reader)
-      reader:write(s .. '\n')
-      print(s)
+      reader:write(s)
       print('Done caching')
     end)
   end)
