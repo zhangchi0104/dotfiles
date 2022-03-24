3d120c0ae3eaf267b9f967ab79d30249edc0c83c
Alex Zhang <zhangchi0104@live.com>
2022-03-24 11:05:59 +0800

    updated plugins

--- nvchad/keymappings/plugins.lua
+++ nvchad/keymappings/plugins.lua
@@ -9,9 +9,9 @@ if utils.has_module("nvim-tree") then
 end
 
 if utils.has_module("trouble") then
-  utils.nnoremap("<leader>xx", "<cmd>Trouble<CR>")
-  utils.nnoremap("<leader>xw", "<cmd>Trouble workspace_diagnostics<CR>")
-  utils.nnoremap("<leader>xd", "<cmd>Trouble document_diagnostics<CR>")
+  utils.nnoremap("<leader>xx", "<cmd>TroubleToggle<CR>")
+  utils.nnoremap("<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>")
+  utils.nnoremap("<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>")
   utils.nnoremap("<leader>xl", "<cmd>Tourble loclist<CR>")
-  utils.nnoremap("<leader>xq", "<cmd>Trouble quickfix<CR>")
+  utils.nnoremap("<leader>xq", "<cmd>TroubleToggle quickfix<CR>")
 end
