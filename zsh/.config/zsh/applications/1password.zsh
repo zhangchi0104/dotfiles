5c3679875355b6d2793dda1a8f8b6b9d939169b9
Alex Zhang <39239965+zhangchi0104@users.noreply.github.com>
2026-02-02 09:14:34 +1100

    update ssh config and skip 1password agent in ssh sessions

--- zsh/.config/zsh/applications/1password.zsh
+++ zsh/.config/zsh/applications/1password.zsh
@@ -1,4 +1,7 @@
 _set_ssh_auth_sock() {
+    # Skip if in SSH session (SSH_AUTH_SOCK is forwarded from client)
+    [[ -n "$SSH_CONNECTION" ]] && return
+
     case "$OSTYPE" in
         darwin*)
             export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
