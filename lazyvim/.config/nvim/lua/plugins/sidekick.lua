49183b78cec93ee8b31e1bce96229e69fc5bc1cc
Alex Zhang <39239965+zhangchi0104@users.noreply.github.com>
2026-02-07 15:44:43 +1100

    update

---
+++ lazyvim/.config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    -- add any options here
+    cli = {
+      mux = {
+        backend = "zellij",
+        enabled = true,
+      },
+    },
+  },
+}
